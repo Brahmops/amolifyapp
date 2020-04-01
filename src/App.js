012282eb122676c076d8ec7794348cafc03ebc29
Brahmops <62913598+Brahmops@users.noreply.github.com>
2020-04-02 03:46:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://redsmith.tk"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CONNECT WITH ME
         </a>
       </header>
     </div>
